41db4c1429bd4921e744e6ed8502037a885cda30
peondusud <reflexion_31@hotmail.com>
2016-06-04 11:54:18 +0200

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+ rm -rf /etc/nginx/snippets
+
+SHELL_PATH=$(dirname $0)
+mv ${SHELL_PATH}/nginx/* /etc/nginx/
+
+
 
 # let's encrypt part
 apt-get update
@@ -10,7 +17,7 @@ sudo git pull
 
 
 
-/opt/letsencrypt/letsencrypt-auto certonly --rsa-key-size 4096 --webroot --webroot-path /var/www/mondomaine.fr -d domain.org
+echo "/opt/letsencrypt/letsencrypt-auto certonly --rsa-key-size 4096 --webroot --webroot-path /var/www/mondomaine.fr -d domain.org"
 
 #certs will be save in /etc/letsencrypt/live/
 ls /etc/letsencrypt/live/
