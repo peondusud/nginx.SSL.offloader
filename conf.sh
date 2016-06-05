b330627560e8787077108699293ccc25e0a2e6ab
peondusud <reflexion_31@hotmail.com>
2016-06-05 02:14:48 +0200

    .

--- conf.sh
+++ conf.sh
@@ -1,66 +1,41 @@
 #!/bin/bash
 
- mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
- rm -rf /etc/nginx/snippets /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-
 SHELL_PATH=$(dirname $0)
+MYDOMAIN=peon.org
+MYMAIL=webmaster@${MYDOMAIN}
+
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+rm -rf /etc/nginx/snippets /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 mv -f ${SHELL_PATH}/nginx/* /etc/nginx/
 mkdir -p /var/spool/nginx
+mkdir -p /var/www/letsencrypt
+mkdir -p /etc/nginx/ssl
+openssl dhparam -out /etc/nginx/ssl/dhparam.pem 1024
+#openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
 
 
-mkdir -p /etc/nginx/ssl/
-openssl dhparam -out /etc/nginx/ssl/dhparam.pem 4096
-
-
-# let's encrypt part
-apt-get update
+# Let's encrypt part
 apt-get install git
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt --depth=1
-cd /opt/letsencrypt
-git pull
-
-
-
-
-echo "/opt/letsencrypt/letsencrypt-auto certonly --rsa-key-size 4096 --webroot --webroot-path /var/www/mondomaine.fr -d domain.org"
-
-#certs will be save in /etc/letsencrypt/live/
-
 mkdir -p /etc/letsencrypt/configs
-"
-# Uncomment and update to generate certificates for the specified domains.
-# domains = example.com, www.example.com
-domains = <mydomain>
+mv ${SHELL_PATH}/letsencrypt.ini /etc/letsencrypt/configs/${MYDOMAIN}.conf
+sed -i "s|<domain>|${MYDOMAIN}|" /etc/letsencrypt/configs/${MYDOMAIN}.conf
+sed -i "s|<domain>|${MYDOMAIN}|" /etc/nginx/conf.d/sslproxy.conf
+sed -i "s|<domain>|${MYDOMAIN}|" /etc/nginx/sites-available/backend.conf
+# disable nginx ssl
+sed -i "s|^\(ssl .*\)on;$|\1off;|" /etc/nginx/conf.d/sslproxy.conf
+ln -s /etc/nginx/sites-available/letsencrypt.conf /etc/nginx/sites-enabled/letsencrypt.conf
 
-# Email used for registration and recovery contact.
-#email = webmaster@gfdg.org
-email = <mymail>
-
-
-rsa-key-size = 4096
-
-# Official server
-#server = https://acme-v01.api.letsencrypt.org/directory
-# staging server to obtain test (invalid) certs
-server = https://acme-staging.api.letsencrypt.org/directory 
-
-# turn off the ncurses UI, we want this to be run as a cronjob
-text = True
-
-# authenticate by placing a file in the webroot (under .well-known/acme-challenge/)
-authenticator = webroot
-webroot-path = /var/www/letsencrypt
-" > /etc/letsencrypt/configs/mydomain.conf
-
-
-
-echo "
-#!/bin/sh
+# Script to renew generated certs
+echo '#!/bin/sh
 for conf in $(ls /etc/letsencrypt/configs/*.conf); do
-  /opt/letsencrypt/letsencrypt-auto --renew --config "$conf" certonly
+  /opt/letsencrypt/letsencrypt-auto renew --config "$conf" certonly
 done
-service nginx reload
-" > /etc/cron.monthly/renew_certs
+service nginx reload' > /etc/cron.monthly/renew_certs
 
-mkdir -p /var/www/letsencrypt
-/opt/letsencrypt/letsencrypt-auto --agree-tos --config /etc/letsencrypt/configs/mydomain.conf certonly 
+/opt/letsencrypt/letsencrypt-auto --agree-tos --config /etc/letsencrypt/configs/${MYDOMAIN}.conf certonly 
+echo "Let's encrypt Certs will be save in /etc/letsencrypt/live/"
+
+# enable nginx ssl
+sed -i "s|^\(ssl .*\)off;$|\1on;|"  /etc/nginx/conf.d/sslproxy.conf
+ln -s /etc/nginx/sites-available/backend.conf /etc/nginx/sites-enabled/backend.conf
